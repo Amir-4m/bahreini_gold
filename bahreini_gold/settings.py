841fc3538a25ef1947db83d175ad5c1e41f73cf0
Amir <amiri.amir30@yahoo.com>
2020-08-29 11:31:23 +0430

    removed static files

--- bahreini_gold/settings.py
+++ bahreini_gold/settings.py
@@ -126,12 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "public", "media_root")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media_root")
 MEDIA_URL = '/media/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
